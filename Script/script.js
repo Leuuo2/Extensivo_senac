5cd44125288b70b27888721b63a886564036a8f3
Leuuo2 <leonardopereira201423@gmail.com>
2025-04-27 16:18:19 -0300

    Extensivo 0.0.1

--- Script/script.js
+++ Script/script.js
@@ -4,3 +4,13 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menulink.classList.toggle('active');
 });
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    }, 
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  }); 
